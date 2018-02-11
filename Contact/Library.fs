63d3605fd8b0b036a2ae0b6205120f6bcb89d8fe
Matthew Winder <mwinder@users.noreply.github.com>
2018-02-11 21:30:11 +0000

    First Contact type + readme

--- Contact/Library.fs
+++ Contact/Library.fs
@@ -1,1 +1,20 @@
 namespace Contact
+
+type Contact = 
+    {
+    FirstName: string;
+    MiddleInitial: string;
+    LastName: string;
+
+    EmailAddress: string;
+    //true if ownership of email address is confirmed
+    IsEmailVerified: bool;
+
+    Address1: string;
+    Address2: string;
+    City: string;
+    State: string;
+    Zip: string;
+    //true if validated against address service
+    IsAddressValid: bool; 
+    }
