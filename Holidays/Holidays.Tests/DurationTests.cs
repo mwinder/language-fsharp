d26f5b5968be2f96bcd70da759806079926895c7
Matthew Winder <mwinder@users.noreply.github.com>
2018-05-15 11:48:09 +0100

    Override addition for now single case Days duration type

---
+++ Holidays/Holidays.Tests/DurationTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace Holidays.Tests
+{
+    public class DurationTests
+    {
+        [Fact]
+        public void Addition()
+        {
+            var result = Days.NewDays(2) + Days.NewDays(4);
+
+            Assert.Equal(Days.NewDays(6), result);
+        }
+    }
+}
--- Holidays/Holidays/Duration.fs
+++ Holidays/Holidays/Duration.fs
@@ -1,5 +1,14 @@
 ﻿namespace Holidays
 
-type Duration =
-    | Days of double
-    | Hours of double
+//type Duration =
+//    | Days of double
+//    | Hours of double
+//with
+//    static member (+) (a, b) =
+//        match a b with
+//            | (Days x) (Days y) -> Days(a + b)
+
+type Days = Days of double
+with
+    static member (+) (a : Days, b : Days) =
+        match a, b with | Days a, Days b -> Days(a + b) 
