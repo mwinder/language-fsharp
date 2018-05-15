b1da516bb3ee06eebf574b02ed36117a54c33114
Matthew Winder <mwinder@users.noreply.github.com>
2018-05-15 12:07:01 +0100

    Ressurected Duration type + added Hours

--- Holidays/Holidays.Tests/DurationTests.cs
+++ Holidays/Holidays.Tests/DurationTests.cs
@@ -5,11 +5,20 @@ namespace Holidays.Tests
     public class DurationTests
     {
         [Fact]
-        public void Addition()
+        public void AdditionDays()
         {
             var result = Days.NewDays(2) + Days.NewDays(4);
 
             Assert.Equal(Days.NewDays(6), result);
+        }
+
+        [Fact]
+        public void AdditionDuration()
+        {
+            Assert.Equal(Duration.NewHours(4), Duration.NewHours(2) + Duration.NewHours(2));
+            Assert.Equal(Duration.NewDays(4), Duration.NewDays(2) + Duration.NewDays(2));
+            Assert.Equal(Duration.NewHours(25), Duration.NewDays(1) + Duration.NewHours(1));
+            Assert.Equal(Duration.NewHours(25), Duration.NewHours(1) + Duration.NewDays(1));
         }
     }
 }
--- Holidays/Holidays/Duration.fs
+++ Holidays/Holidays/Duration.fs
@@ -1,13 +1,20 @@
 ﻿namespace Holidays
 
-//type Duration =
-//    | Days of double
-//    | Hours of double
-//with
-//    static member (+) (a, b) =
-//        match a b with
-//            | (Days x) (Days y) -> Days(a + b)
+type Duration =
+    | Days of double
+    | Hours of double
+with
+    static member (+) (a, b) =
+        match a, b with
+            | (Days a, Days b) -> Days(a + b)
+            | (Hours a, Hours b) -> Hours(a + b)
+            | (Days a, Hours b) -> Hours(24.0*a + b)
+            | (Hours a, Days b) -> Hours(a + 24.0*b)
 
 type Days = Days of double
 with
-    static member (+) (Days a, Days b) = Days(a + b) 
+    static member (+) (Days a, Days b) = Days(a + b)
+
+type Hours = Hours of double
+with
+    static member (+) (Hours a, Hours b) = Hours(a + b) 
