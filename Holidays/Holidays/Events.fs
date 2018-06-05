4e6f11c83631c16ec8c4329625dacb4213f74475
Matthew Winder <mwinder@users.noreply.github.com>
2018-06-05 08:54:29 +0100

    Added DeclineData

--- Holidays/Holidays/Events.fs
+++ Holidays/Holidays/Events.fs
@@ -2,8 +2,12 @@
 
 open Holidays
 
+type DeclineData = {
+    Reason : string
+}
+
 type HolidayEvent =
     | Requested of HolidayId
     | Approved of HolidayId
-    | Declined of HolidayId
+    | Declined of HolidayId * DeclineData
     | Cancelled of HolidayId
